44b21f1401d9130345b2e0d88312571c0a61a2dc
iamshubham2001 <myshubham2001@gmail.com>
2024-09-01 15:52:52 +0530

    Print Function

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello World")
-print(5)
+# print --> function
+print(7)
+print("hello world")
+print("hii", 8)
+print("hey i am a good boy \n and smart looking guy")
+
+# \n --> escape sequence character :: new line character
+
+print("hey i am a \"good boy\"")
+
+print("hey", 6,7, sep="~", end="009\n")
+print("harry")
