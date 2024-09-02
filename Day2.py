1b4c12adf9a7954a8a87254ae1d85fcab1743a5e
iamshubham2001 <myshubham2001@gmail.com>
2024-09-02 14:16:16 +0530

    Variables & Data types

---
+++ Day2.py
@@ -0,0 +1,21 @@
+# Variable && Data types
+# variabe is like a container that holds Data
+# Data types specific the type of value a variable holds.
+# In python we can print the type of any operator using type function:
+a=1
+print(a)
+a1 = 9
+b = "harry" 
+print(b)
+print(a+a1)
+d=None
+c=True
+print("the type of a is", type(a))
+print("the type of a is", type(b))
+print("the type of a is", type(d))
+print("the type of a is", type(c))
+
+# Numeric Data type : int, float, Complex
+# Text data : str
+# Boolean data : True,False
+# Sequenced data : list, tuple
