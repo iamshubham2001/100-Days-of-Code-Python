f2a605b68b2f42c060bcdfe5fe9cd556faa8acf0
iamshubham2001 <myshubham2001@gmail.com>
2024-09-02 14:52:24 +0530

    Day2

--- Day2.py
+++ Day2.py
@@ -2,7 +2,9 @@
 # variabe is like a container that holds Data
 # Data types specific the type of value a variable holds.
 # In python we can print the type of any operator using type function:
-a=1
+
+'''
+a=complex(8,2)
 print(a)
 a1 = 9
 b = "harry" 
@@ -14,8 +16,22 @@ print("the type of a is", type(a))
 print("the type of a is", type(b))
 print("the type of a is", type(d))
 print("the type of a is", type(c))
+'''
 
 # Numeric Data type : int, float, Complex
 # Text data : str
 # Boolean data : True,False
 # Sequenced data : list, tuple
+# Mapped data : dict
+
+
+# Calculator
+
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/6)
+print(5//3)     #Floor Division
+print(5%6)    #Modulus
+print(5**6)  # Exponential
+
---
+++ Exercise1.py
@@ -0,0 +1,16 @@
+# Create a Calculator
+
+x = 20
+y = 7
+ans1 = x + y
+print("addition of",x,"and",y,"is : ", ans1)
+ans2 = x - y
+print("Subtraction of",x,"and",y,"is : ", ans2)
+ans3 = x * y
+print("Multiplication of",x,"and",y,"is : ", ans3)
+ans4 = x / y
+print("Division of",x,"and",y,"is : ", ans4)
+ans5 = x // y
+print("Floor Division of",x,"and",y,"is : ", ans5)
+ans6 = x % y
+print("Modulous of",x,"and",y,"is : ", ans6)
