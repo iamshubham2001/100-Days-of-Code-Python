c9ac1987ed12892a40cf081e05e7bdd85055670a
iamshubham2001 <myshubham2001@gmail.com>
2024-09-03 17:38:03 +0530

    String

--- Day3.py
+++ Day3.py
@@ -21,7 +21,23 @@ print(a.count("Harry"))     # Count
 
 '''
 
-# find() method
+# find() method  --> it searches the first occurance of the given value and returns the index where it is present.
 
 str1 = "He's name is Dan. He is an honest man."
 print(str1.find("is"))
+print(str1.index("is"))
+
+# index is same as find but it not return -1
+
+
+# isalnum() --> Method return true only if the entire string only consists of A-Z, a-z, 0-9. If any other characters or punctuations are present, then it return false.
+
+str1= "WelcomeTOTheConsole"
+print(str1.isalnum())
+
+# islower()
+
+
+# isprintable()  --> Return true if all the values within the given string are printable,if not, then return False.
+# swapcase
+# title
