c506be77b690a702da7c8b5fde453f7902963c83
iamshubham2001 <myshubham2001@gmail.com>
2024-09-12 22:34:51 +0530

    Day8

--- Day8.py
+++ Day8.py
@@ -1,6 +1,20 @@
 # String formating in python
 
+'''
 letter = "Hey i am from {} and I am from {}"
 country = "India"
 name = "Shubham"
-print(letter.format(name,country))
+
+#print(letter.format(name,country))
+
+print(f"Hey my name is {name} and I am from {country}")
+'''
+
+'''
+txt = "for only{price:.2f} dollars"
+print(txt.format(price=49.01009))
+'''
+
+price =49.02304
+txt = f"For only {price: .2f} dollars!"
+print(txt)
