fd848462603425606d06226b64340ecd7ed98715
iamshubham2001 <myshubham2001@gmail.com>
2024-09-16 15:42:30 +0530

    Day9

--- Day8.py
+++ Day8.py
@@ -15,6 +15,19 @@ txt = "for only{price:.2f} dollars"
 print(txt.format(price=49.01009))
 '''
 
+'''
 price =49.02304
 txt = f"For only {price: .2f} dollars!"
 print(txt)
+'''
+
+# Docstrings
+'''
+    Python docstring are the string literals that appear right after the defination of a function, method , class or module.
+'''
+
+def square(n):
+    ''' Takes the number n, return the square of n'''
+    print(n**2)
+square(5)
+print(square.__doc__)
