6a2cb1371cc02cd288fad6e7c914424e082abe0a
iamshubham2001 <myshubham2001@gmail.com>
2024-09-18 18:19:38 +0530

    Day10

--- Day10.py
+++ Day10.py
@@ -9,6 +9,8 @@ s1.update(s2)           # update
 print(s1,s2)
 '''
 
+
+'''
 cities1={"Tokyo","Madrid","Berlin","Delhi"}
 cities2={"Tokyo","Seoul","Kabul","Madrid"}
 cities3=(cities1.union(cities2))
@@ -16,7 +18,27 @@ print(cities3)
 cities4=(cities1.intersection(cities2))   # intersection
 print(cities4)
 
-# symmetric Difference
+# symmetric_Difference
 
 cities5=(cities1.symmetric_difference(cities2))
 print(cities5)
+
+# isdisjoint()
+cities6=(cities1.isdisjoint(cities2))
+print(cities6)
+
+# issuperset()
+
+print(cities1.issubset(cities2))
+print(cities1.add("Bihar"))
+
+# Remove---->>Discard
+# del cities
+# clear cities
+'''
+
+info={"shubham","raju",19,False,5.9,"chintu"}
+if "shubham" in info:
+    print("yes")
+else:
+    print("no")
